f54d1d4c55b8402b7b529c0a426daeab1c2736a3
Jhasterina <85872607+Jhasterina@users.noreply.github.com>
2022-01-11 15:43:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,26 @@ function setup() {
 function draw() {
 	image(video, 0, 0, 600, 500);
 }
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
+}
+
+function modelLoaded() {
+	console.log('posenet is working');
+}
